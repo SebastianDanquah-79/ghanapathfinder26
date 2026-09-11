86a15957c0f748b3c91222463d1ce436c4c88b7c
SebastianDanquah-79 <tumikwabena79@gmail.com>
2026-09-11 02:35:32 +0000

    Add Opportunity Graph to My Path

--- src/routes/my-path.tsx
+++ src/routes/my-path.tsx
@@ -1,13 +1,19 @@
 import { createFileRoute } from "@tanstack/react-router";
 import MyPath from "@/pages/MyPath";
 import WorldClassPathLayer from "@/components/WorldClassPathLayer";
+import OpportunityGraph from "@/components/OpportunityGraph";
 
 function MyPathRoute() {
   return (
     <>
       <MyPath />
-      <div className="w-full max-w-6xl mx-auto px-4 sm:px-6 lg:px-8 pb-24 md:pb-12">
+      <div className="w-full max-w-6xl mx-auto px-4 sm:px-6 lg:px-8 space-y-5 pb-24 md:pb-12">
         <WorldClassPathLayer />
+        <OpportunityGraph
+          goal=""
+          routes={[]}
+          savedCounts={{ university: 0, scholarship: 0, internship: 0 }}
+        />
       </div>
     </>
   );
