962c8b77a3777785b57f98e70c7e694cb4cf542f
SebastianDanquah-79 <tumikwabena79@gmail.com>
2026-09-11 02:32:48 +0000

    Integrate world-class decision layer into My Path

--- src/routes/my-path.tsx
+++ src/routes/my-path.tsx
@@ -1,6 +1,18 @@
 import { createFileRoute } from "@tanstack/react-router";
 import MyPath from "@/pages/MyPath";
+import WorldClassPathLayer from "@/components/WorldClassPathLayer";
+
+function MyPathRoute() {
+  return (
+    <>
+      <MyPath />
+      <div className="w-full max-w-6xl mx-auto px-4 sm:px-6 lg:px-8 pb-24 md:pb-12">
+        <WorldClassPathLayer />
+      </div>
+    </>
+  );
+}
 
 export const Route = createFileRoute("/my-path")({
-  component: MyPath,
+  component: MyPathRoute,
 });
