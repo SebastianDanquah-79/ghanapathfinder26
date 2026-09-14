6777636d4f022a78bdc197ca411f1e574a065d17
SebastianDanquah-79 <tumikwabena79@gmail.com>
2026-09-14 03:35:19 +0000

    Remove Life Simulator route

--- src/routes/life-simulator.tsx
+++
@@ -1,6 +0,0 @@
-import { createFileRoute } from "@tanstack/react-router";
-import LifeSimulator from "@/pages/LifeSimulator";
-
-export const Route = createFileRoute("/life-simulator")({
-  component: LifeSimulator,
-});
