aff525da784486222e3b9323c06e26f902ecef80
SebastianDanquah-79 <tumikwabena79@gmail.com>
2026-09-18 22:04:09 +0000

    fix: remove ungenerated international route from production build

--- src/routes/international-admissions.tsx
+++
@@ -1,6 +0,0 @@
-import { createFileRoute } from "@tanstack/react-router";
-import InternationalAdmissions from "@/pages/InternationalAdmissions";
-
-export const Route = createFileRoute("/international-admissions")({
-  component: InternationalAdmissions,
-});
