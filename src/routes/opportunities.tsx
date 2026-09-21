de52ca32e6e1715f3a1fb85e6b64336356b2671e
SebastianDanquah-79 <tumikwabena79@gmail.com>
2026-09-21 05:43:22 +0000

    debug: remove isolated opportunities route from failing build

--- src/routes/opportunities.tsx
+++
@@ -1,3 +0,0 @@
-import { createFileRoute } from "@tanstack/react-router";
-import Opportunities from "@/pages/Opportunities";
-export const Route=createFileRoute("/opportunities")({component:Opportunities});
