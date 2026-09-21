2beb659c4f174aaa11039e30facf12870c5e9cf2
SebastianDanquah-79 <tumikwabena79@gmail.com>
2026-09-21 04:18:38 +0000

    feat: add global opportunity platform surfaces

---
+++ src/routes/opportunities.tsx
@@ -0,0 +1,1 @@
+import { createFileRoute } from "@tanstack/react-router"; import Opportunities from "@/pages/Opportunities"; export const Route=createFileRoute("/opportunities")({component:Opportunities});
