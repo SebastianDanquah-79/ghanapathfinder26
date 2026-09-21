20148dcd6fac8765abf2814fb183188d1309e134
SebastianDanquah-79 <tumikwabena79@gmail.com>
2026-09-21 05:31:20 +0000

    feat: add opportunities route

---
+++ src/routes/opportunities.tsx
@@ -0,0 +1,3 @@
+import { createFileRoute } from "@tanstack/react-router";
+import Opportunities from "@/pages/Opportunities";
+export const Route=createFileRoute("/opportunities")({component:Opportunities});
