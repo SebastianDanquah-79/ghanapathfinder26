0928a44e188b96f514bbfdda2c26e19b07d8e2b1
SebastianDanquah-79 <tumikwabena79@gmail.com>
2026-09-21 06:52:33 +0000

    Add Africa leadership reference route

---
+++ src/routes/africa-leadership.tsx
@@ -0,0 +1,3 @@
+import { createFileRoute } from "@tanstack/react-router";
+import AfricaLeadership from "@/pages/AfricaLeadership";
+export const Route = createFileRoute("/africa-leadership")({ component: AfricaLeadership });
