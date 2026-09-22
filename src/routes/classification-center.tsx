ea95e4935d1f88c4acda39281e10a2236f11382e
SebastianDanquah-79 <tumikwabena79@gmail.com>
2026-09-22 05:43:50 +0000

    Add navigable platform section route

---
+++ src/routes/classification-center.tsx
@@ -0,0 +1,1 @@
+import { createFileRoute } from "@tanstack/react-router"; import { SectionHub } from "@/pages/Platform"; export const Route=createFileRoute("/classification-center")({component:()=> <SectionHub section="classification"/>});
