2c5bd1dad2a16c1707af4618f164e539d4e03a16
SebastianDanquah-79 <tumikwabena79@gmail.com>
2026-09-22 18:03:33 +0000

    feat: add dedicated portal routes

---
+++ src/routes/innovation.tsx
@@ -0,0 +1,1 @@
+import { createFileRoute } from "@tanstack/react-router"; import { Innovation } from "@/pages/ContentHubs"; export const Route=createFileRoute("/innovation")({component:Innovation});
