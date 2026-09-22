ba4261c0cca0f7f8fe32d85f17c74b1e8bd296a2
SebastianDanquah-79 <tumikwabena79@gmail.com>
2026-09-22 03:34:04 +0000

    Add CV builder route

---
+++ src/routes/cv-builder.tsx
@@ -0,0 +1,6 @@
+import { createFileRoute } from "@tanstack/react-router";
+import CVBuilder from "@/pages/CVBuilder";
+
+export const Route = createFileRoute("/cv-builder")({
+  component: CVBuilder,
+});
