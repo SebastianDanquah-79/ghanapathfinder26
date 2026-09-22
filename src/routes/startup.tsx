81b13780a069a0c5d6e4b5f5806198560dfa7863
SebastianDanquah-79 <tumikwabena79@gmail.com>
2026-09-22 05:49:13 +0000

    Route Startup directly to Startup Founder Dashboard

--- src/routes/startup.tsx
+++ src/routes/startup.tsx
@@ -1,1 +1,1 @@
-import { createFileRoute } from "@tanstack/react-router"; import { SectionHub } from "@/pages/Platform"; export const Route=createFileRoute("/startup")({component:()=> <SectionHub section="startup"/>});
+import { createFileRoute } from "@tanstack/react-router"; import { FounderDashboard } from "@/pages/Platform"; export const Route=createFileRoute("/startup")({component:FounderDashboard});
