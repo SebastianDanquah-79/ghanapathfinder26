3e8fa4b81b9d38aede92c3b9e91e1cb662d525e5
SebastianDanquah-79 <tumikwabena79@gmail.com>
2026-09-22 17:21:47 +0000

    feat: add directory route

---
+++ src/routes/employer-directory.tsx
@@ -0,0 +1,1 @@
+import { createFileRoute } from "@tanstack/react-router"; import { EmployerDirectory } from "@/pages/Directories"; export const Route=createFileRoute("/employer-directory")({component:EmployerDirectory});
