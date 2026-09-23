851f452dfc8117151d6fc15dab2e3c5089622c27
SebastianDanquah-79 <tumikwabena79@gmail.com>
2026-09-23 17:08:56 +0000

    feat: surface opportunity intelligence on homepage

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -9,6 +9,7 @@ import ExploreGrid from "@/components/ExploreGrid";
 import AnnouncementBanner from "@/components/AnnouncementBanner";
 import WhyGhanaPathFinder from "@/components/WhyGhanaPathFinder";
 import PathfinderDecisionHub from "@/components/PathfinderDecisionHub";
+import OpportunityIntelligence from "@/components/OpportunityIntelligence";
 
 const ImpactSection = lazy(() => import("@/components/ImpactSection"));
 const CareerSection = lazy(() => import("@/components/CareerSection"));
@@ -52,6 +53,7 @@ const Index = () => (
 
     <WhyGhanaPathFinder />
     <PathfinderDecisionHub />
+    <OpportunityIntelligence />
     <ExploreGrid />
     <CollegeRecommender />
     <UniversityDirectory />
