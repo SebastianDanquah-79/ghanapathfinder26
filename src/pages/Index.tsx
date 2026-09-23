6c31100a35aaea4a24b1611b91f91db0be1e961d
SebastianDanquah-79 <tumikwabena79@gmail.com>
2026-09-23 17:11:39 +0000

    feat: show live platform intelligence on homepage

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -9,6 +9,7 @@ import ExploreGrid from "@/components/ExploreGrid";
 import AnnouncementBanner from "@/components/AnnouncementBanner";
 import WhyGhanaPathFinder from "@/components/WhyGhanaPathFinder";
 import PathfinderDecisionHub from "@/components/PathfinderDecisionHub";
+import PlatformOverview from "@/components/PlatformOverview";
 
 const ImpactSection = lazy(() => import("@/components/ImpactSection"));
 const CareerSection = lazy(() => import("@/components/CareerSection"));
@@ -52,6 +53,7 @@ const Index = () => (
 
     <WhyGhanaPathFinder />
     <PathfinderDecisionHub />
+    <PlatformOverview />
     <ExploreGrid />
     <CollegeRecommender />
     <UniversityDirectory />
