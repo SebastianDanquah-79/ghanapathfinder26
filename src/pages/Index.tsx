66c33af2ae4851429897f676bbb22c679154647c
SebastianDanquah-79 <tumikwabena79@gmail.com>
2026-09-23 18:44:06 +0000

    feat: personalize existing student homepage

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -10,6 +10,7 @@ import AnnouncementBanner from "@/components/AnnouncementBanner";
 import WhyGhanaPathFinder from "@/components/WhyGhanaPathFinder";
 import PathfinderDecisionHub from "@/components/PathfinderDecisionHub";
 import PlatformOverview from "@/components/PlatformOverview";
+import StudentPulse from "@/components/StudentPulse";
 
 const ImpactSection = lazy(() => import("@/components/ImpactSection"));
 const CareerSection = lazy(() => import("@/components/CareerSection"));
@@ -51,6 +52,7 @@ const Index = () => (
       <HeroSection />
     </div>
 
+    <StudentPulse />
     <WhyGhanaPathFinder />
     <PathfinderDecisionHub />
     <PlatformOverview />
