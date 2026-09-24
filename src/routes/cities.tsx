b715741d504949755f43e64d60af68143e1482b4
SebastianDanquah-79 <tumikwabena79@gmail.com>
2026-09-24 20:56:14 +0000

    feat: add global Africa discovery routes

---
+++ src/routes/cities.tsx
@@ -0,0 +1,1 @@
+import { createFileRoute } from "@tanstack/react-router"; import GlobalDiscovery from "@/pages/GlobalDiscovery"; export const Route=createFileRoute("/cities")({component:()=> <GlobalDiscovery kind="cities"/>});
