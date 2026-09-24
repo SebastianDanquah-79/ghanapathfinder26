7e0a63882474c3e2a1e2cd9170233334561e54ac
SebastianDanquah-79 <tumikwabena79@gmail.com>
2026-09-24 20:56:23 +0000

    feat: add global Africa discovery routes

---
+++ src/routes/opportunity-radar.tsx
@@ -0,0 +1,1 @@
+import { createFileRoute } from "@tanstack/react-router"; import GlobalDiscovery from "@/pages/GlobalDiscovery"; export const Route=createFileRoute("/opportunity-radar")({component:()=> <GlobalDiscovery kind="radar"/>});
