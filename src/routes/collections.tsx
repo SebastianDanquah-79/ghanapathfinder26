b156c2f6bb2a99a149df7f49b3168d82b67f6ddf
SebastianDanquah-79 <tumikwabena79@gmail.com>
2026-09-24 20:33:40 +0000

    fix: avoid adding an ungenerated route to the production tree

--- src/routes/collections.tsx
+++
@@ -1,4 +0,0 @@
-import { createFileRoute } from "@tanstack/react-router";
-import Collections from "@/pages/Collections";
-
-export const Route = createFileRoute("/collections")({ component: Collections });
