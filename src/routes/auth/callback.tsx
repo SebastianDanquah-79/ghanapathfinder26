7b9462fdb683b85e4593e0b94abe66d0864476fa
SebastianDanquah-79 <tumikwabena79@gmail.com>
2026-09-26 02:56:44 +0000

    feat: add GhanaPathFinder routes

---
+++ src/routes/auth/callback.tsx
@@ -0,0 +1,1 @@
+import { createFileRoute } from "@tanstack/react-router"; import AuthCallback from "@/pages/AuthCallback"; export const Route=createFileRoute("/auth/callback")({component:AuthCallback});
