5e61b7da6c3c189b694d298d1adf490924107979
SebastianDanquah-79 <tumikwabena79@gmail.com>
2026-09-26 01:23:54 +0000

    feat: add discovery routes

---
+++ src/routes/opportunities.tsx
@@ -0,0 +1,3 @@
+import { createFileRoute } from "@tanstack/react-router";
+import OpportunityRadar from "@/pages/OpportunityRadar";
+export const Route = createFileRoute("/opportunities")({ component: OpportunityRadar });
