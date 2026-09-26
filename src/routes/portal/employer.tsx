4c26352c64fb2295ee262850fe1fa819b3851400
SebastianDanquah-79 <tumikwabena79@gmail.com>
2026-09-26 02:56:30 +0000

    feat: add GhanaPathFinder routes

---
+++ src/routes/portal/employer.tsx
@@ -0,0 +1,1 @@
+import { createFileRoute } from "@tanstack/react-router"; import EmployerPortal from "@/pages/EmployerPortal"; export const Route=createFileRoute("/portal/employer")({component:EmployerPortal});
